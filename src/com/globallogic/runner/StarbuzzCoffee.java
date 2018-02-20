9a7da4f002fdea1d92b9c00f5423d1978acc7576
alexandroid1 <alex123456>
2018-02-20 13:29:29 +0200

    perform main method

--- src/com/globallogic/runner/StarbuzzCoffee.java
+++ src/com/globallogic/runner/StarbuzzCoffee.java
@@ -12,21 +12,12 @@ public class StarbuzzCoffee {
 
     public static void main(String[] args) {
         Beverage beverage = new Espresso();
-        System.out.println(beverage.getDescription()
-                + " $" + beverage.cost());
+        System.out.println(beverage.getDescription() + " $" + beverage.cost());
 
-        Beverage beverage2 = new DarkRoast();
-        beverage2 = new Mocha(beverage2);
-        beverage2 = new Mocha(beverage2);
-        beverage2 = new Whip(beverage2);
-        System.out.println(beverage2.getDescription()
-                + " $" + beverage2.cost());
+        Beverage beverage2 = new Whip(new Mocha(new Mocha(new DarkRoast())));
+        System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
 
-        Beverage beverage3 = new HouseBlend();
-        beverage3 = new Soy(beverage3);
-        beverage3 = new Mocha(beverage3);
-        beverage3 = new Whip(beverage3);
-        System.out.println(beverage3.getDescription()
-                + " $" + beverage3.cost());
+        Beverage beverage3 = new Whip(new Mocha(new Soy(new HouseBlend())));
+        System.out.println(beverage3.getDescription() + " $" + beverage3.cost());
     }
 }
